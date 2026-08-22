85ba49883046fae1698d6ffbe4ee83047c6084f9
YASHWARDHAN RATHOD <yashwardhanrathod7@gmail.com>
2026-08-22 21:57:39 +0530

    Delete member5 directory

--- member5/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Member 5 cybersecurity module."""
-
-from .cybersecurity import analyze_input
-
-__all__ = ["analyze_input"]
--- member5/cybersecurity/__init__.py
+++
@@ -1,5 +0,0 @@
-"""ScamShield cybersecurity analysis package."""
-
-from .analyzer import analyze_input
-
-__all__ = ["analyze_input"]
